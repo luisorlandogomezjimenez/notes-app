53ddf46f445ccc7c116c7182644c7e36c9cc3e06
Orlando <luisorlandogomezjimenez@gmail.com>
2021-08-26 20:37:15 -0500

    models, handlebars

--- src/database.js
+++ src/database.js
@@ -1,1 +1,13 @@
 //Connect databases (mongoose model)
+const mongoose = require("mongoose");
+
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log("Database is connected"))
+  .catch((err) => console.log(err));
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 //Server code (express code)
 
 const express = require("express");
+const exphbs = require("express-handlebars");
 const path = require("path");
 
 //Initializations
@@ -9,6 +10,16 @@ const app = express();
 //Settings
 app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
 //Middlewares
 app.use(express.urlencoded({ extended: false }));
@@ -17,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.get("/", (req, res) => {
-  res.send("hello hello");
+  res.render("index");
 });
 
 //Static files
